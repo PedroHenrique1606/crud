db853c281825f3399ef1fff22050aa156ab9c779
PedroHenrique1606 <pedromelosilva11911@gmail.com>
2024-04-13 17:04:25 -0300

    feat: methods update and delete

--- database-postgres.js
+++ database-postgres.js
@@ -3,28 +3,39 @@ import { sql } from './db.js';
 
 export class DatabasePostgress {
     async create(users) {
-        // const userId = uuidv4();
-        const {name, category, cell, email, password, route} = users
+        const { name, category, cell, email, password, route } = users;
 
-        await sql `insert into users (name, category, cell, email, password, route) VALUES (${name},${category},${cell},${email},${password},${route})`
+        await sql`
+            INSERT INTO users (name, category, cell, email, password, route)
+            VALUES (${name}, ${category}, ${cell}, ${email}, ${password}, ${route})
+        `;
     }
 
     async list(search) {
-        let users
-        if(search){
-            users = await sql`select * from users where name ilike "%${search}%"`
-        } else{
-            users = await sql`select * from users`
+        let users;
+        if (search) {
+            users = await sql`SELECT * FROM users WHERE name ILIKE "%${search}%"`;
+        } else {
+            users = await sql`SELECT * FROM users`;
         }
 
-        return users
+        return users;
     }
 
-    update(id, users) {
-        
+    async update(id, userData) {
+        const { name, category, cell, email, password, route } = userData;
+
+        await sql`
+            UPDATE users
+            SET name = ${name}, category = ${category}, cell = ${cell}, email = ${email}, password = ${password}, route = ${route}
+            WHERE id = ${id}
+        `;
     }
 
-    delete(id) {
-        
+    async delete(id) {
+        await sql`
+            DELETE FROM users
+            WHERE id = ${id}
+        `;
     }
 }
